00ed27a43d9ca9bbee8d2179e6580aaa10d25ac5
Administrator <admin@local.host>
2015-01-20 13:15:58 +0100

    <zx<zx

---
+++ ClavyHTMLExportPlugin/src/fdi/ucm/server/exportparser/html/oda/HTMLprocess.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package fdi.ucm.server.exportparser.html.oda;
+
+import java.util.ArrayList;
+
+/**
+ * @author Joaquin Gayoso-Cabada
+ *
+ */
+public class HTMLprocess {
+
+	public HTMLprocess(ArrayList<Long> listaDeDocumentos,
+			ArrayList<Long> ListaDeAdministracion, boolean administrador) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void preocess() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
